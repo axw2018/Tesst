9b7d7153f9ab44730ab1e05510682a0be0ffee45
a <22>
2018-10-30 18:41:44 +0800

    添加Test

---
+++ Test20181030/src/User.java
@@ -0,0 +1,41 @@
+/**
+ *
+ */
+public class User {
+    private String username;
+    private String age;
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", age='" + age + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
